b0d38568a87fee8262a33ba9156af63dbeb01db2
JooHo Kim <artipect123@gmail.com>
2023-03-29 04:57:42 +0900

    clean

--- main.js
+++ main.js
@@ -1,43 +1,45 @@
 let target = document.querySelector("#dynamic");
-let stringArr = ["Learn to HTML","Learn to CSS","Learn to Javascript","Learn to Python","Learn to Ruby"];
-
+let stringArr = [
+  "Learn to HTML",
+  "Learn to CSS",
+  "Learn to Javascript",
+  "Learn to Python",
+  "Learn to Ruby",
+];
 
 // 커서 깜빡임 효과
-function blink(){
+function blink() {
   target.classList.toggle("active");
 }
 setInterval(blink, 500);
 
 // 랜덤으로 문자열 산출
-function randomString(){
+function randomString() {
   let randomIndex = Math.floor(Math.random() * stringArr.length);
-  let selectString = stringArr[randomIndex]; 
+  let selectString = stringArr[randomIndex];
   let selectStringArr = selectString.split("");
 
   return selectStringArr;
 }
 
-function resetTyping(){
+function resetTyping() {
   target.textContent = "";
   dynamic(randomString());
 }
 
 // 한글자씪 텍스트 출력
-  function dynamic(randomArr){
-    if(randomArr.length>0)
-    {
-      target.textContent+=randomArr.shift();
-      setTimeout(()=>{
-        dynamic(randomArr); 
-      }, 80);
-    }
-    else{
-      setTimeout(resetTyping, 3000);
-    }
-  };
+function dynamic(randomArr) {
+  if (randomArr.length > 0) {
+    target.textContent += randomArr.shift();
+    setTimeout(() => {
+      dynamic(randomArr);
+    }, 80);
+  } else {
+    setTimeout(resetTyping, 3000);
+  }
+}
 
-  dynamic(randomString());
-
+dynamic(randomString());
 
 //Test
 // function dynamic(randomArr){
